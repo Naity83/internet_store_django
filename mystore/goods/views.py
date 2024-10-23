b6e223e7d8f4edbfb6115e83fb99ed2a65dfc567
Nataliya Khomenko <NataliaHomenko83@gmail.com>
2024-10-24 00:51:09 +0300

    search

--- mystore/goods/views.py
+++ mystore/goods/views.py
@@ -1,16 +1,30 @@
 from django.shortcuts import render
 from .models import Products
 from django.core.paginator import Paginator
+from .utils import q_search
 
-def catalog(request, category_slug, page=1):
-    
+def catalog(request, category_slug = None):
+    page = request.GET.get('page', 1)
+    on_sale = request.GET.get('on_sale', None)
+    order_by = request.GET.get('order_by', None)  # Get the current page number or default to 1 if not provided in the URL query string.
+    query = request.GET.get('q', None)
+
     if category_slug == 'all':
         goods = Products.objects.all()
+    elif query:
+        goods = q_search(query)
     else:        
         goods = Products.objects.filter(category__slug=category_slug)
 
+
+    if on_sale:
+        goods = goods.filter(discount__gt=0)
+
+    if order_by and order_by != "default":
+        goods = goods.order_by(order_by)
+
     paginator = Paginator(goods, 2)
-    current_page = paginator.page(page)
+    current_page = paginator.page(int(page))
 
     context = {
         
